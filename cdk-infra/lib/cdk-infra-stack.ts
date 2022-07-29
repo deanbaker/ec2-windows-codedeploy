b15fb030af1e36fe1b1d16e98676331337efab99
Dean Baker <deano.baker@gmail.com>
2022-07-29 13:47:05 +1000

    Deploying with CodeDeploy and full LoadBalancing

--- cdk-infra/lib/cdk-infra-stack.ts
+++ cdk-infra/lib/cdk-infra-stack.ts
@@ -1,8 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as s3 from 'aws-cdk-lib/aws-s3'
-import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import iam = require('aws-cdk-lib/aws-iam');
+import s3 = require('aws-cdk-lib/aws-s3')
+import ec2 = require('aws-cdk-lib/aws-ec2')
+import autoscaling = require('aws-cdk-lib/aws-autoscaling');
+import elbv2 = require('aws-cdk-lib/aws-elasticloadbalancingv2')
+import codedeploy = require('aws-cdk-lib/aws-codedeploy')
 
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -11,8 +14,8 @@ export class CdkInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
- 
-
+    // Existing keypair to use
+    const keypair = 'tutorial-rdp-keypair'
 
     // VPC to deploy
     const vpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', { isDefault: true })
@@ -22,12 +25,14 @@ export class CdkInfraStack extends Stack {
 
     // AMI to use
     const webServerAMI = new ec2.LookupMachineImage({
-      name: 'nameaspnet-web-server',
+      name: 'aspnet-web-server',
       windows: true
     })
 
     // Where we will host our artefacts to depoloy
-    const deployBucket = new s3.Bucket(this, 'MyDeployableAssets');
+    const deployBucket = new s3.Bucket(this, 'MyDeployableAssets', {
+      publicReadAccess: false
+    });
 
     // Role for the EC2 instance to download the deployable artefact
     const ec2Role = new iam.Role(this, 'ec2-s3-readonly', {
@@ -37,7 +42,34 @@ export class CdkInfraStack extends Stack {
       ]
     });
 
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.LARGE),
+      machineImage: webServerAMI,
+      securityGroup: webserverSG,
+      role: ec2Role,
+      keyName: keypair,
+      associatePublicIpAddress: true,
+      desiredCapacity: 2,
+    });
 
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'lb', {
+      vpc,
+      internetFacing: true
+    });
 
+    const httpListener = lb.addListener("http", {
+      port: 80
+    })
+
+    const targetGroup = httpListener.addTargets("TG", {
+      port: 80,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      targets: [asg]
+    })
+
+    const deploymentGroup = new codedeploy.ServerDeploymentGroup(this, 'DeploymentGroup', {
+      autoScalingGroups: [asg]
+    });
   }
 }
